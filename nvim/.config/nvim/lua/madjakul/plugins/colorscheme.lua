27ac796a76cf0c041a167a2ced4cb3a58b673580
Madjakul <francis.kulumba@inria.fr>
2025-01-09 15:39:07 +0100

    feat: changing theme to gruvbox default

--- nvim/.config/nvim/lua/madjakul/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/madjakul/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     priority = 1000,
     config = function()
         require("gruvbox").setup({
-            contrast = "hard", -- can be "hard", "soft" or empty string
+            contrast = "", -- can be "hard", "soft" or empty string
         })
         vim.cmd("colorscheme gruvbox")
     end,
