708a665be9663612bd0b7668ff565837ed5b8ace
Madjakul <francis.kulumba@inria.fr>
2025-04-24 20:05:21 +0200

    refactor: changing colorscheme from medium to soft

--- nvim/.config/nvim/lua/madjakul/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/madjakul/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     priority = 1000,
     config = function()
         require("gruvbox").setup({
-            contrast = "", -- can be "hard", "soft" or empty string
+            contrast = "soft", -- can be "hard", "soft" or empty string
         })
         vim.cmd("colorscheme gruvbox")
     end,
