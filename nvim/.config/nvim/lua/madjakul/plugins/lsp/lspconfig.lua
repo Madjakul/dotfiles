5b675a8cfe9a568e1b6a31da1f5ee500cdf700b4
Madjakul <francis.kulumba@inria.fr>
2025-12-01 12:59:06 +0100

    Migrate LSP config to nvim 0.11+

--- nvim/.config/nvim/lua/madjakul/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/madjakul/plugins/lsp/lspconfig.lua
@@ -14,44 +14,26 @@ return {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
                 local opts = { buffer = ev.buf, silent = true }
-                vim.api.nvim_set_hl(0, "@lsp.type.class", { link = "@type.class" })
-
                 opts.desc = "Show LSP references"
                 keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-
                 opts.desc = "Go to declaration"
                 keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
                 opts.desc = "Show LSP definitions"
                 keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-
                 opts.desc = "Show LSP implementations"
                 keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-
                 opts.desc = "Show LSP type definitions"
                 keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-
                 opts.desc = "See available code actions"
                 keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
                 opts.desc = "Smart rename"
                 keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
                 opts.desc = "Show buffer diagnostics"
                 keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-
                 opts.desc = "Show line diagnostics"
                 keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-
-                opts.desc = "Go to previous diagnostic"
-                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-
-                opts.desc = "Go to next diagnostic"
-                keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
                 opts.desc = "Show documentation for what is under cursor"
                 keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
                 opts.desc = "Restart LSP"
                 keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
             end,
@@ -59,11 +41,24 @@ return {
 
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
+        -- Configure diagnostics
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = " ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                },
+            },
+            virtual_text = {
+                spacing = 4,
+                prefix = "■",
+            },
+            float = {
+                source = true,
+            },
+        })
 
         -- Default config for all servers
         local default_config = {
@@ -85,6 +80,7 @@ return {
         }
 
         for _, server in ipairs(servers) do
+            -- FIXED: Pass server name as first argument, options as second
             vim.lsp.config(server, default_config)
         end
 
