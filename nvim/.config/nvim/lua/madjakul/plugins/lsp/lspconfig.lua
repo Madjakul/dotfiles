62692c1fd21acc773dbdf71f2844c1b9089a716c
Madjakul <francis.kulumba@inria.fr>
2025-12-02 19:13:00 +0100

    fx: too many characters for diagnostic symbols

--- nvim/.config/nvim/lua/madjakul/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/madjakul/plugins/lsp/lspconfig.lua
@@ -62,10 +62,10 @@ return {
         vim.diagnostic.config({
             signs = {
                 text = {
-                    [vim.diagnostic.severity.ERROR] = "  ",
-                    [vim.diagnostic.severity.WARN] = "  ",
-                    [vim.diagnostic.severity.HINT] = "  ",
-                    [vim.diagnostic.severity.INFO] = "  ",
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = " ",
+                    [vim.diagnostic.severity.INFO] = " ",
                 },
             },
             virtual_text = { spacing = 4, prefix = "■" },
